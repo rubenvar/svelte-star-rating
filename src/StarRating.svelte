7e35db6088cdc9458f7c5dd2db4238527ae6ae96
Rubén <hey@rubenvara.io>
2021-03-21 21:49:00 +0100

    make rating display reactive throw error if not valid reting  value

--- src/StarRating.svelte
+++ src/StarRating.svelte
@@ -6,25 +6,35 @@
   export let id = makeUniqueId();
   export let rating;
   export let style = '';
-  // TODO check if rating prop is number and between 0 and 5
-  // maybe something in the line of:
-  // if (!isNumber(rating) || rating < 0 || rating > 5) throw new Error();
   export let config = {};
+  // check if rating prop is number and between 0 and 5
+  $: if (!isNumber(rating) || rating < 0 || rating > 5) {
+    throw new Error('rating value is not valid! 🙅‍♀️');
+  }
+
   // number of full, 'half' and empty stars
-  const full = Math.floor(rating);
-  const half = Math.round((rating - full) * 100) / 100;
-  const empty = Math.floor(5 - rating);
+  $: full = Math.floor(rating);
+  $: half = Math.round((rating - full) * 100) / 100;
+  $: empty = Math.floor(5 - rating);
   // partial arrays
-  const fullArr = Array(full).fill(1);
-  const halfArr = half !== 0 ? [half] : [];
-  const emptyArr = Array(empty).fill(0);
+  $: fullArr = Array(full).fill(1);
+  $: halfArr = half !== 0 ? [half] : [];
+  $: emptyArr = Array(empty).fill(0);
   // array of star-to-be numbers
-  const stars = fullArr.concat(halfArr).concat(emptyArr);
+  $: stars = fullArr.concat(halfArr).concat(emptyArr);
+  // TODO do all this array thing a little more efficiently, maybe?
+
   // font size of rating text will be half of the star size, with a min value of 16px
   let fontSize = config.size && isNumber(config.size) ? config.size : 20;
   fontSize = fontSize / 2 < 16 ? 16 : fontSize / 2;
-  // TODO do all this array thing a litter more efficiently
 </script>
+
+<div {style}>
+  {#each stars as star}
+    <Star {id} full={star} {config} />
+  {/each}
+  {#if config.showText}<span style="font-size:{fontSize}px">{rating}</span>{/if}
+</div>
 
 <style>
   div {
@@ -38,10 +48,3 @@
     margin-left: 8px;
   }
 </style>
-
-<div {style}>
-  {#each stars as star}
-    <Star {id} full={star} {config} />
-  {/each}
-  {#if config.showText}<span style="font-size:{fontSize}px">{rating}</span>{/if}
-</div>
